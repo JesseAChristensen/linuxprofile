7cdade083b7e26459e325fe412cc17de7892c335
Jesse <jesse.christensen@imsar.com>
2017-02-08 15:47:00 -0700

    modified:   install.sh   added non-interactive option to install script args

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+helpmsg(){
+  echo -e "usage:" 
+  echo -e "-f : force install without user interaction"
+}
+
 HOMEFILES=(
 ".bashrc"
 ".gitconfig"
@@ -7,6 +12,15 @@ HOMEFILES=(
 ".tmux.conf"
 ".vimrc"
 )
+
+if [[ "$1" == "-f" ]]; then
+  for each_file in ${HOMEFILES[@]}; do
+    cp -f $each_file ~/
+  done
+  exit 0
+fi
+
+
 echo -e "This will install the following files into $HOME, overwriting existing files:"
 for each_file in ${HOMEFILES[@]}; do
   echo $each_file
