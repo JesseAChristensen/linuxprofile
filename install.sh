3b90a5323860ad161b7dc27ca93bbc97f59f54dd
Jesse <jesse.christensen@imsar.com>
2017-02-08 13:26:33 -0700

    modified:   install.sh   fixed install only copying .bashrc

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 HOMEFILES=(
-.bashrc
-.gitconfig
-.inputrc
-.lftprc
-.tmux.conf
-.vimrc
+".bashrc"
+".gitconfig"
+".inputrc"
+".lftprc"
+".tmux.conf"
+".vimrc"
 )
 for each_file in $HOMEFILES; do
   cp -i $each_file ~/
