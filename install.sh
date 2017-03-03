4e02bc7439f7ba8c766cb0606ddec956218e3a21
Jesse Chrisetnsen <jesse.christensen@imsar.com>
2017-03-03 20:59:32 +0000

    modified:   install.sh   added option to cat .gitcommittemplate to existing file

--- install.sh
+++ install.sh
@@ -37,6 +37,19 @@ fi
 for each_file in ${HOMEFILES[@]}; do
   cp -i $each_file ~/
 done
+
+if [[ ! -e ~/.gitcommittemplate ]]; then
+  echo "Adding '.gitcommittemplate'..."
+  cp -i .gitcommittemplate ~/
+else
+  echo "~/.gitcommittemplate already exists... would you like to add to the existing template?"
+  echo "[N/y]?"
+  read COMMIT
+  if $(echo $COMMIT | grep -qP "[Yy][Ee[Ss]?]?"); then
+    cat .gitcommittemplate >> ~/.gitcommittemplate
+  fi
+fi
+
 mkdir -p ~/.vim/ftplugin
 cp -if .vim/ftplugin/* ~/.vim/ftplugin/
 
